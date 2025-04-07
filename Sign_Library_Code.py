937804395a206ba2667a03abfdc736ba4f35e889
dodojr75 <jcn030@email.latech.edu>
2025-04-06 22:40:20 -0500

    Add files via upload

---
+++ Sign_Library_Code.py
@@ -0,0 +1,135 @@
+from PIL import Image
+
+def signA(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\a.png")
+
+def signB(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\b.png")
+
+def signC(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\c.png")
+
+def signD(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\d.png")
+
+def signE(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\e.png")
+
+def signF(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\f.png")
+
+def signG(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\g.png")
+
+def signH(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\h.png")
+
+def signI(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\i.png")
+
+def signJ(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\j.png")
+
+def signK(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\k.png")
+
+def signL(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\l.png")
+
+def signM(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\m.png")
+
+def signN(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\n.png")
+
+def signO(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\o.png")
+
+def signP(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\p.png")
+
+def signQ(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\q.png")
+
+def signR(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\r.png")
+
+def signS(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\s.png")
+
+def signT(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\t.png")
+
+def signU(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\u.png")
+
+def signV(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\v.png")
+
+def signW(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\w.png")
+
+def signX(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\x.png")
+
+def signY(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\y.png")
+
+def signZ(x):
+    Image.open(r"C:\Users\Jayden Needham\Desktop\Comp Sci\CSC 132\Assignments\Cyber Expo Test Folder\FingerSigns\z.png")
+
+def signFinder(x):
+    li1 = list(x)
+    for i in li1:
+        if (i == 'a'):
+            signA(i)
+        elif (i == 'b'):
+            signB(i)
+        elif (i == 'c'):
+            signC(i)
+        elif (i == 'd'):
+            signD(i)
+        elif (i == 'e'):
+            signE(i)
+        elif (i == 'f'):
+            signF(i)
+        elif (i == 'g'):
+            signG(i)
+        elif (i == 'h'):
+            signH(i)
+        elif (i == 'i'):
+            signI(i)
+        elif (i == 'j'):
+            signJ(i)
+        elif (i == 'k'):
+            signK(i)
+        elif (i == 'l'):
+            signL(i)
+        elif (i == 'm'):
+            signM(i)
+        elif (i == 'n'):
+            signN(i)
+        elif (i == 'o'):
+            signO(i)
+        elif (i == 'p'):
+            signP(i)
+        elif (i == 'q'):
+            signQ(i)
+        elif (i == 'r'):
+            signR(i)
+        elif (i == 's'):
+            signS(i)
+        elif (i == 't'):
+            signT(i)
+        elif (i == 'u'):
+            signU(i)
+        elif (i == 'v'):
+            signV(i)
+        elif (i == 'w'):
+            signW(i)
+        elif (i == 'x'):
+            signX(i)
+        elif (i == 'y'):
+            signY(i)
+        elif (i == 'z'):
+            signZ(i)
